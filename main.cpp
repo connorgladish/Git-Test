e2ea46d999cce6efb90198831db9ee407ccd4c7f
Connor Gladish <cgladish@ut.utm.edu>
2024-08-31 20:10:51 -0500

    working main

--- main.cpp
+++ main.cpp
@@ -3,9 +3,23 @@
 
 using namespace std;
 
+
+int sum(int n){
+  int total = 0;
+  for(int i=1;i<=n;i++){
+    total += i;
+  }
+  return total;
+}
+
 int main() {
 
-  cout << "Hello World!";
+  int n;
+  cout << "Enter a number: ";
+  cin >> n;
+  int result = sum(n);
+
+  cout << "The sum of the numbers from 1 to " << n << " is " << result << endl;
   
   return 0;
 }
