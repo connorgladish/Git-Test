78bb4aadca7fdd9fbc86d6633e3b998f4277ad0d
Connor Gladish <cgladish@ut.utm.edu>
2024-08-31 20:24:36 -0500

    working product func

--- main.cpp
+++ main.cpp
@@ -12,14 +12,22 @@ int sum(int n){
   return total;
 }
 
+int product(int n){
+  int total = 1;
+  for (int i=1;i<=n;i++){
+    total *= i;
+  }
+  return total;
+}
+
 int main() {
 
   int n;
   cout << "Enter a number: ";
   cin >> n;
-  int result = sum(n);
+  int result = product(n);
 
-  cout << "The sum of the numbers from 1 to " << n << " is " << result << endl;
+  cout << "The product of the numbers from 1 to " << n << " is " << result << endl;
   
   return 0;
 }
